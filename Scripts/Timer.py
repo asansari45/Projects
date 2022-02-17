cd321c970c83b92b81a4c108beed364c300ccb01
asansari45 <asansari45@hotmail.com>
2022-02-16 22:46:39 -0500

    Added new Timer.py script.

--- Scripts/Timer.py
+++ Scripts/Timer.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from datetime import *
 import calendar
 from dateutil.relativedelta import *
+import sys
 
 class TimerInfo:
     column = 0
@@ -10,23 +11,17 @@ class TimerInfo:
         self._name = name
 
         # create the frame and label
-        self._frame = tk.Frame(master=window)
-        self._label = tk.Label(master=self._frame, text=text, font=("Arial", 25), foreground="white", background=bg)
+        self._frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+        self._label = tk.Label(master=self._frame, text='%s\n%s'%(text,name), font=("Arial", 25))
         self._label.pack()
         self._frame.grid(row = 0, column=TimerInfo.column)
-
-        self._labelFrame = tk.Frame(master=window)
-        self._labelLabel = tk.Label(master=self._frame, text=name, font=("Arial", 25), foreground="white", background=bg)
-        self._labelLabel.pack()
-        self._frame.grid(row=1, column=TimerInfo.column)
         TimerInfo.column += 1
 
         
 class Timer:
-    def __init__(self, title):
+    def __init__(self, title, endTime):
         self._window = tk.Tk()
         self._window.title(title)
-        self._counter = 0
         self._fields = { 
             'year':    TimerInfo(self._window, 'YEARS ',    '0000', 'blue'),
             'months':  TimerInfo(self._window, 'MONTHS',  '00', 'green'),
@@ -38,19 +33,19 @@ class Timer:
         self._window.after(1000, self.update)
 
         # retirement date
-        self._finalDate = datetime.fromisoformat('2029-09-30')
+        self._finalDate = datetime.fromisoformat(endTime)
 
         self._window.mainloop()
 
     def update(self):
         r = relativedelta(self._finalDate, datetime.now())
-        self._fields['seconds']._label.config(text='{seconds:2d}'.format(seconds=r.seconds))
-        self._fields['minutes']._label.config(text='{minutes:2d}'.format(minutes=r.minutes))
-        self._fields['hours']._label.config(text='{hours:2d}'.format(hours=r.hours))
-        self._fields['days']._label.config(text='{days:2d}'.format(days=r.days))
-        self._fields['months']._label.config(text='{months:2d}'.format(months=r.months))
-        self._fields['year']._label.config(text='{year:4d}'.format(year=r.years))
+        self._fields['seconds']._label.config(text='{seconds:2d}\nSECS'.format(seconds=r.seconds))
+        self._fields['minutes']._label.config(text='{minutes:2d}\nMINS'.format(minutes=r.minutes))
+        self._fields['hours']._label.config(text='{hours:2d}\nHRS'.format(hours=r.hours))
+        self._fields['days']._label.config(text='{days:2d}\nDAYS'.format(days=r.days))
+        self._fields['months']._label.config(text='{months:2d}\nMONTHS'.format(months=r.months))
+        self._fields['year']._label.config(text='{year:4d}\nYEARS'.format(year=r.years))
         self._window.after(1000, self.update)
 
-timer1 = Timer('Amir S. Ansari Retirement Time')
+timer1 = Timer(sys.argv[1], sys.argv[2])
 
